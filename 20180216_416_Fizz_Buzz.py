dda87dc13e6e8d5ce684a52e0139ffbfe77b1be0
Tiana_Lan <lantnju@163.com>
2018-02-16 13:28:24 +0800

    Create 20180216_416_Fizz_Buzz.py
    The code of problem 416 named Fizz Buzz in LeetCode.

---
+++ 20180216_416_Fizz_Buzz.py
@@ -0,0 +1,18 @@
+class Solution:
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        result = []
+        for i in range(1,1+n):
+            if i % 15 == 0:
+                result.append('FizzBuzz')
+            elif i % 5 == 0:
+                result.append('Buzz')
+            elif i % 3 == 0:
+                result.append('Fizz')
+            else:
+                result.append(str(i))
+        
+        return result
