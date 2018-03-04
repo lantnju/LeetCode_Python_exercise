9df10eba08f2eb93639a206362842520b15c351c
Tiana_Lan <lantnju@163.com>
2018-03-04 09:20:56 +0800

    Create 20180304_389_Find_the_Difference.py
    The code of problem 389_Find_the_Difference.

---
+++ 20180304_389_Find_the_Difference.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        s = list(s)
+        for i in t:
+            if i not in s:
+                return i
+            else:
+                s.remove(i)
