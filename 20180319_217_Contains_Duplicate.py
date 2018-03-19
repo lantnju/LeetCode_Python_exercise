bf362197fe6eba1f97b31029dc7be045d046432b
Tiana_Lan <lantnju@163.com>
2018-03-19 09:27:37 +0800

    Create 20180319_217_Contains_Duplicate.py
    The code of problem 217_Contains_Duplicate.

---
+++ 20180319_217_Contains_Duplicate.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        a = set(nums)
+        if len(a) < len(nums):
+            return True
+        else:
+            return False
